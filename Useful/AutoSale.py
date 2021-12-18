c2dad408a8b0a5b7eedcb89af824490e35f7f776
Deforeturn <deforeturn@gmail.com>
2021-12-18 13:49:50 +0900

    modify

--- Useful/AutoSale.py
+++ Useful/AutoSale.py
@@ -10,9 +10,9 @@ upbit = pyupbit.Upbit(access_key, secret_key)
 def upbitSale1(result):
     price_crnt = pyupbit.get_current_price(["KRW-BTC"])     # 현재가.
     balance = upbit.get_balance(ticker="KRW-BTC")     # 보유 수량.
-    if result >= 0.87:
+    if result[1] >= 0.9 and result[0] <= 0.25:
         print("매수 : ", upbit.buy_market_order("KRW-BTC", (price_crnt * balance) / 30))  # 해당 코인의 보유 가치의 3%만큼 매수.
-    elif result <= 0.15:
+    elif result[0] >= 0.8 and result[1] <= 0.25:
         print("매도 : ", upbit.sell_limit_order("KRW-BTC", price_crnt, balance / 30))  # 해당 보유 코인의 3% 갯수만큼 시장가에 매도.
 
 def upbitSale2(result):
@@ -21,4 +21,14 @@ def upbitSale2(result):
     if result[1] >= 0.8 and result[0] <= 0.3:
         print("매수 : ", upbit.buy_market_order("KRW-BTC", (price_crnt * balance) / 30))  # 해당 코인의 보유 가치의 3%만큼 매수.
     elif result[0] >= 0.7 and result[1] <= 0.3:
+        print("매도 : ", upbit.sell_limit_order("KRW-BTC", price_crnt, balance / 30))  # 해당 보유 코인의 3% 갯수만큼 시장가에 매도.
+
+
+# Softmax용.
+def upbitSale3(result):
+    price_crnt = pyupbit.get_current_price(["KRW-BTC"])     # 현재가.
+    balance = upbit.get_balance(ticker="KRW-BTC")     # 보유 수량.
+    if result >= 0.87:
+        print("매수 : ", upbit.buy_market_order("KRW-BTC", (price_crnt * balance) / 30))  # 해당 코인의 보유 가치의 3%만큼 매수.
+    elif result <= 0.15:
         print("매도 : ", upbit.sell_limit_order("KRW-BTC", price_crnt, balance / 30))  # 해당 보유 코인의 3% 갯수만큼 시장가에 매도.
